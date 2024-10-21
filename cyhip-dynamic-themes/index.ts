40a81d02751a938f7432775d8cc7c1206c5d17df
Kassio R Ferreira <kassio_rf@hotmail.com>
2024-10-21 01:52:31 -0300

    export consistentChroma

--- cyhip-dynamic-themes/index.ts
+++ cyhip-dynamic-themes/index.ts
@@ -1,3 +1,4 @@
+export { consistentChroma } from "./lib/tw-dynamic-themes/runtime";
 export { dynamicTwClasses } from "./lib/tw-dynamic-themes/twPlugin";
 export { hueScheme as defaultHueScheme } from "./src/hue-scheme";
 export { currentAccentValue, getThemeProperties } from "./src/theme-helpers";
