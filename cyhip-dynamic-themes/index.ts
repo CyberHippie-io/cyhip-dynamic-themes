9423102c0dd2cd7b054a42a2dd08be17da2f2842
Kassio R Ferreira <kassio_rf@hotmail.com>
2024-10-21 18:00:41 -0300

    pkg: export pkg version

--- cyhip-dynamic-themes/index.ts
+++ cyhip-dynamic-themes/index.ts
@@ -3,3 +3,5 @@ export { dynamicTwClasses } from "./lib/tw-dynamic-themes/twPlugin";
 export { hueScheme as defaultHueScheme } from "./src/hue-scheme";
 export { currentAccentValue, getThemeProperties } from "./src/theme-helpers";
 export { useColorTheme } from "./src/theme-hook";
+
+export { version } from "./__version__";
