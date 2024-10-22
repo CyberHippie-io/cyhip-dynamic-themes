204fefd8729aab87aa94ba6d62f79ed4d51a5db5
Kassio R Ferreira <kassio_rf@hotmail.com>
2024-10-22 20:12:34 -0300

    feat: added to init files the theme-switcher.tsx component

--- cyhip-dynamic-themes/src/cli.ts
+++ cyhip-dynamic-themes/src/cli.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 import chalk from "chalk";
 import fs from "fs";
 import inquirer from "inquirer";
@@ -7,16 +6,7 @@ import path from "path";
 import huePalettes from "./_templates/hue-scheme.js";
 import rootCss from "./_templates/root-css.js";
 import themeColors from "./_templates/theme-colors.js";
-
-// import chalk from "chalk";
-// import fs from "fs";
-// import inquirer from "inquirer";
-// import path from "path";
-// import { fileURLToPath } from "url";
-
-// const huePalettes = require("./_templates/hue-palettes");
-// const rootCss = require("./_templates/root-css");
-// const themeColors = require("./_templates/theme-colors");
+import themeSwitcher from "./_templates/theme-switcher.js";
 
 const initThemesDirectory = (themesDir: string) => {
     if (!fs.existsSync(themesDir)) {
@@ -31,20 +21,26 @@ const initThemesDirectory = (themesDir: string) => {
             path.join(themesDir, "theme-colors.ts"),
             themeColors.trim()
         );
+        fs.writeFileSync(
+            path.join(themesDir, "theme-switcher.tsx"),
+            themeSwitcher.trim()
+        );
 
         console.log(chalk.green(`Default files created in "${themesDir}".`));
 
         console.log(
             chalk.cyan(
-                `  - Set your color palettes in "/themes/hue-palettes.ts".`
+                `  1. Update your tailwind.conf.ts as described in /themes/theme-colors.ts`
             )
         );
         console.log(
-            chalk.cyan(`  - Customize your theme in "/themes/root.css".`)
+            chalk.cyan(
+                `  2. Import /themes/root.css into the base tsx of your application, e.g.: [ Main.tsx or App.tsx or Layout.tsx ]`
+            )
         );
         console.log(
             chalk.cyan(
-                `  - See how to enable accent vars on tailwind.config.ts in "/themes/theme-colors.ts".`
+                `  3. Add the /themes/theme-switcher.tsx component to your application and to see how it works.`
             )
         );
     } else {
