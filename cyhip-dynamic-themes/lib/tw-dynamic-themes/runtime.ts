a97b23a55d5c91293f3d94762d1c51105f078d96
Kassio R Ferreira <kassio_rf@hotmail.com>
2024-11-03 15:02:19 -0300

    add chromaData as parameter on runtime methods

--- cyhip-dynamic-themes/lib/tw-dynamic-themes/runtime.ts
+++ cyhip-dynamic-themes/lib/tw-dynamic-themes/runtime.ts
@@ -10,6 +10,8 @@ import { makeVariable, shades } from "./common";
 
 const toGamut = _toGamut as (...args: unknown[]) => (color: string) => Color;
 
+
+
 /**
  * A map of CSS varable name to color
  */
@@ -18,27 +20,19 @@ type SingleVariable = [string, string];
 export function getVariables({
     baseName,
     hue,
-    mode = "consistent",
+    chromaData = chromaDataDefault,
 }: {
     baseName: string;
     hue: number;
-    mode?: "bright" | "consistent";
+    chromaData?: Record<number,number>;
 }): SingleVariable[] {
-    const whitePalette = hue == -1;
-    const calculator = mode === "bright" ? highestChroma : consistentChroma;
-    return shades.map((shade, shadeIndex) => [
-        makeVariable({ name: baseName, shade }),
-        calculator(shadeIndex, +hue, whitePalette),
+    const variables: SingleVariable[] = shades.map((shade, shadeIndex) => [
+        makeVariable({name: baseName, shade}),
+        consistentChroma(shadeIndex, hue, chromaData),
     ]);
-}
+    return variables;
+};
 
-export function updateVariables(variables: SingleVariable[], el?: HTMLElement) {
-    const target = el ?? document.documentElement;
-
-    for (const [varName, value] of variables) {
-        target.style.setProperty(varName, value + "");
-    }
-}
 
 const lightnessForShade = (shade: number) => {
     const highestL = 100;
@@ -54,63 +48,42 @@ const lightnessForShade = (shade: number) => {
 };
 const lightness = shades.map(lightnessForShade);
 
-export const highestChroma = (shadeIndex: number, hue: number) => {
-    const oklch = converter("oklch");
-
-    // Setting an obsurdly high chroma
-    const color = `oklch(${lightness[shadeIndex]} 0.4 ${hue})`;
-
-    // Clamping it to the highest chroma possible
-    return serializeColor(
-        oklch(toGamut("p3", "oklch", differenceEuclidean("oklch"), 0)(color))
-    );
-};
 
 export const consistentChroma = (
-    i: number,
+    shadeIndex: number,
     hue: number,
-    whitePalette: boolean = false
+    chromaData: Record<number,number> = chromaDataDefault,
+
 ) => {
+    
     const oklch = converter("oklch");
 
+    const asWhite = hue === -1;
+
     // Using a pinned chroma
-    const chroma = whitePalette ? chromaData[i] * 0.00001 : chromaData[i];
-    const light = whitePalette ? lightness[i] * 1.0 : lightness[i] * 0.95;
+    const chroma = !asWhite ? chromaData[shadeIndex] : 0.0;
+    const light =  !asWhite ? lightness[shadeIndex] * 0.95 : lightness[shadeIndex];  
 
-    const color = `oklch(${light} ${chroma} ${hue})`;
-
+    const color = `oklch(${light} ${chroma} ${+hue})`;
     return serializeColor(
         oklch(toGamut("p3", "oklch", differenceEuclidean("oklch"), 0)(color))
-    );
-};
-
-// const chromaData: Record<number, number> = {
-//     0: 0.0108,
-//     1: 0.0321,
-//     2: 0.0609,
-//     3: 0.0908,
-//     4: 0.1398,
-//     5: 0.1472,
-//     6: 0.1299,
-//     7: 0.1067,
-//     8: 0.0898,
-//     9: 0.0726,
-//     10: 0.054,
-// };
-
-const chromaData: Record<number, number> = {
-    0: 0.0114,
-    1: 0.0331,
-    2: 0.0774,
-    3: 0.1275,
-    4: 0.1547,
-    5: 0.1355,
-    6: 0.1164,
-    7: 0.0974,
-    8: 0.0782,
-    9: 0.0588,
-    10: 0.0491,
-};
+    );    
+}
 
 const serializeColor = (c: Oklch): string =>
     `${c.l.toFixed(3)} ${c.c.toFixed(3)} ${c.h?.toFixed(3)}`;
+
+
+const chromaDataDefault: Record<number, number> = {
+    0: 0.0108,
+    1: 0.0121,
+    2: 0.0609,
+    3: 0.0908,
+    4: 0.1398,
+    5: 0.1472,
+    6: 0.1299,
+    7: 0.1067,
+    8: 0.0898,
+    9: 0.0726,
+    10: 0.005,
+};
