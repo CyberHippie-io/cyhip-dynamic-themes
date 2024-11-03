e5781c3229adc7ace0527ec51520ba95f7682036
Kassio R Ferreira <kassio_rf@hotmail.com>
2024-11-03 15:03:23 -0300

    add theme provider

---
+++ cyhip-dynamic-themes/src/theme-provider.tsx
@@ -0,0 +1,29 @@
+// 'use client'; // enable on Nextjs
+
+import { updateTheme, useTheme } from './theme-hook';
+import React, { useEffect, useState, type ReactNode } from 'react';
+import { ThemeConfig } from './theme.config';
+
+interface ThemeProviderProps {
+    children: ReactNode;
+    themeConfig: ThemeConfig;
+}
+
+const ThemeProvider: React.FC<ThemeProviderProps> = ({ children, themeConfig }) => {
+    /**@TODO Get this values from user cookie */
+    const { theme } = useTheme();
+    const [isInitialized, setIsInitialized] = useState(false);
+
+    useEffect(() => {
+        if (!isInitialized) {
+            setIsInitialized(true);
+            updateTheme(themeConfig);
+        }
+    }, [theme, isInitialized]);
+
+    if (!isInitialized) return null;
+
+    return <>{children}</>;
+};
+
+export { ThemeProvider };
