fe38e6ebaea4aa5b9bb98dd3a6c7a5bdc1008974
Kassio R Ferreira <kassio_rf@hotmail.com>
2024-11-04 22:53:50 -0300

    update basic-usage app to v.0.2.2

--- examples/basic-usage/src/main.tsx
+++ examples/basic-usage/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./themes/root.css";
+import { ThemeConfig, ThemeProvider } from 'cyhip-dynamic-themes';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './themes/root.css';
+import { chromaData, hueScheme } from './themes/theme.config.ts';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <ThemeProvider
+            themeConfig={
+                {
+                    hue: hueScheme.blue,
+                    mode: 'light',
+                    chromaData: chromaData,
+                } as ThemeConfig
+            }
+        >
+            <App />
+        </ThemeProvider>
     </StrictMode>
 );
